f7d0e219cb2a082ad49e44a3b1172254eb36a35b
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2023-04-08 08:54:58 +0530

    Updated Episode 5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,61 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
+  
 const Body = () => {
-  const [mylist,setMylist]=useState(resList);
-  return (
+  const[allRes,setAllRes]=useState([]);
+  const [filteredmylist, setFilteredMylist] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  useEffect(()=>{
+    getRestaurants();
+  },[])
+
+  async function getRestaurants(){
+    const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6628779&lng=77.4648192&page_type=DESKTOP_WEB_LISTING")
+    const json= await data.json();
+    setFilteredMylist(json?.data?.cards[2]?.data?.data?.cards);
+    setAllRes(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  if(filteredmylist?.length===0) return <h1>No Restaurant Matches Your Search</h1>
+  return allRes?.length === 0 ? (<Shimmer/>):(
     <div>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = allRes.filter((res)=>res?.data?.name?.toLowerCase()?.includes(searchInput.toLowerCase()))
+            setFilteredMylist(data);
+          }
+          
+        }
+        >
+          Search
+        </button>
+      </div>
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            const newList=mylist.filter((res)=> res.data.avgRating >= 4)
-            setMylist(newList)
+            const newList = allRes.filter((res) => res.data.avgRating >= 4);
+            setFilteredMylist(newList);
           }}
-          
         >
           Top Rated Restaurant's
-        </button>
-      </div>
+        </button> 
+       </div>
       <div className="res-container">
-        {mylist.map((restaurant) => (
+        {filteredmylist.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
