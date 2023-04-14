e67e28089b8a1497c8e99091fd47afda7c97d663
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2023-04-14 12:59:02 +0530

    Class based

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,26 @@
 import { Outlet } from "react-router-dom";
-const About=()=>{
-    return(
-        <div>
-            <h1>About us</h1>
-            <p>This is React for you.</p>
-            <Outlet/>
-        </div>
+import Profile from "./Profile";
+import ProfileClass from "./ProfileClass";
+import React from "react";
+class About extends React.Component {
+    constructor(props){
+        super(props);
+        
+    }
+    componentDidMount(){
+        
+        console.log("DidMount")
+    }
+  render() {
+    console.log("render")
+    return (
+      <div>
+        <h1>About us</h1>
+        <p>This is React for you.</p>
+        
+        <ProfileClass/>
+      </div>
     );
+  }
 }
 export default About;
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -1,7 +1,36 @@
 import React from "react";
 class ProfileClass extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            userInfo:{
+                name:"Dummy Name",
+                location:"Dummy Location",
+            }
+        }
+        console.log("constructor");
+    }
+    async componentDidMount(){
+        const data= await fetch("https://api.github.com/users/Anutyagi007");
+        const json=await data.json();
+        this.setState({
+            userInfo:json,
+        })
+
+        console.log("componentDidMount")
+    }
     render(){
-        return <h1>Profile Class Component</h1>
+        
+        return (
+            <div>
+                <h1>Profile Class Component</h1>
+                <img src={this.state.userInfo.avatar_url} alt="" />
+                <h2>Name:{this.state.userInfo.name}</h2>
+                <h2>Location:{this.state.userInfo.location}</h2>
+                
+            </div>
+        )
+        
     }
 }
 
