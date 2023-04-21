e7cca066ff70de343ac10bb70799ee86be3a9065
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2023-04-22 01:22:54 +0530

    Updated API for Menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -11,38 +11,33 @@ const RestaurantMenu = () => {
   }, []);
   async function getRestaurantInfo() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6628779&lng=77.4648192&restaurantId="+resId+"&submitAction=ENTER"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId="+resId+""
     );
     const json = await data.json();
-    console.log(json.data.cards[0].card.card.info);
-    setRes(json.data.cards[0].card.card.info);
+    console.log(json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards)
+    setRes(json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards);
   }
   return (
     <div>
       <div>
         <h1>Restaurant id:-{resId}</h1>
         <h2>{res.name}</h2>
-        <img src={CDN_URL + res.cloudinaryImageId} alt="" />
+        <img src={CDN_URL + res.imageId} alt="" />
         <h2>{res.areaName}</h2>
         <h3>{res.city}</h3>
         <h3>{res.costForTwoMessage}</h3>
         <h3>{res.avgRating} Stars</h3>
         <p>{res.cuisines}</p>
       </div>
+      <div >
       <h1>Menu</h1>
       <ul>
-        <li>Paneer Masala</li>
-        <li>Paneer Masala</li>
-        <li>Paneer Masala</li>
-        <li>Paneer Masala</li>
-        <li>Paneer Masala</li>
-        <li>Paneer Masala</li>
-        <li>Paneer Masala</li>
-        <li>Paneer Masala</li>
-        <li>Paneer Masala</li>
+        {
+          res.map((item)=><li key={item.card.info.id}>{item.card.info.name}</li>)
+        }
       </ul>
+          </div>
     </div>
   );
 };
-//ccrgpubt2dvjsqvxktfl
 export default RestaurantMenu;
